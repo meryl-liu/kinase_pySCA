4fe28d9042457ff16cfd906b1973e71fa573973c
Dongyang Li <dongyanglibio@gmail.com>
2023-06-30 16:42:17 -0700

    correct the bug for mapping positions

--- MSA_pos_mapping_functions.py
+++ MSA_pos_mapping_functions.py
@@ -11,35 +11,42 @@ def get_sequence(uniprot_id):
     
     return seq
 
-def msa_pos_mapping(full_seq, sub_seq):
+def msa_pos_mapping(full_seq, sub_seq, cutoff=4):
     """
     This function takes in the full protein sequence `full_seq` and the sequence as given in
     the MSA entry `sub_seq`, and returns the positions of all residues appeared in the full
     length sequence.
     """
-    sub_seq = sub_seq.replace("-", "").upper()
-    full_seq = full_seq.upper()
+    sub_seq = sub_seq.replace("-", "").replace("\n","").upper()
+    full_seq = full_seq.replace("\n","").upper()
 
     full_seq_pos = 0  # Initialize position in full sequence (1-based)
     #print(full_seq_pos)
     sub_seq_pos = 0  # Initialize position in subsequence (1-based)
     mappings = []  # List to hold position mappings
-
+    
+    # set a cutoff to require at least certain number of continuous residues. 
+    continuity_cutoff = 4
     while full_seq_pos < len(full_seq) and sub_seq_pos < len(sub_seq):
         # find initial matches
         while  (full_seq_pos < len(full_seq) and sub_seq[sub_seq_pos] != full_seq[full_seq_pos]):
             #print(full_seq_pos)
             full_seq_pos += 1
             continue
-        mappings.append(full_seq_pos)
+        # mappings.append(full_seq_pos)
+        tentative_mappings = [] # List to hold the most recent continuous pos mappings.
+        tentative_mappings.append(full_seq_pos)
         while (
             full_seq_pos < len(full_seq)
             and sub_seq_pos < len(sub_seq)
             and sub_seq[sub_seq_pos] == full_seq[full_seq_pos]
         ):
+            tentative_mappings.append(full_seq_pos)
             sub_seq_pos += 1
             full_seq_pos += 1
-            mappings.append(full_seq_pos)
+        if len(tentative_mappings) >= continuity_cutoff:
+            for res_id in tentative_mappings:
+                mappings.append(res_id)
     mappings = [x+1 for x in mappings]
     mappings_string = '\n'.join(str(element) for element in mappings)
     return mappings_string
