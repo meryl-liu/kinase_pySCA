39dc0761e41751fd10c222a4ef15a659922ec086
Meryl Liu <meryl.liu@gmail.com>
2023-06-30 16:17:14 -0700

    Updated position functions

--- MSA_pos_mapping_functions.py
+++ MSA_pos_mapping_functions.py
@@ -2,7 +2,7 @@ import requests as r
 
 def get_sequence(uniprot_id):
     """get sequence from UniProt for a given UniProt ID, return as a string"""
-    
+    #print(uniprot_id)
     baseUrl="http://www.uniprot.org/uniprot/"
     currentUrl=baseUrl+uniprot_id+".fasta"
     response = r.post(currentUrl)
@@ -21,14 +21,14 @@ def msa_pos_mapping(full_seq, sub_seq):
     full_seq = full_seq.upper()
 
     full_seq_pos = 0  # Initialize position in full sequence (1-based)
+    #print(full_seq_pos)
     sub_seq_pos = 0  # Initialize position in subsequence (1-based)
     mappings = []  # List to hold position mappings
 
     while full_seq_pos < len(full_seq) and sub_seq_pos < len(sub_seq):
         # find initial matches
-        while sub_seq[sub_seq_pos] != full_seq[full_seq_pos] and full_seq_pos < len(
-            full_seq
-        ):
+        while  (full_seq_pos < len(full_seq) and sub_seq[sub_seq_pos] != full_seq[full_seq_pos]):
+            #print(full_seq_pos)
             full_seq_pos += 1
             continue
         mappings.append(full_seq_pos)
@@ -41,12 +41,14 @@ def msa_pos_mapping(full_seq, sub_seq):
             full_seq_pos += 1
             mappings.append(full_seq_pos)
     mappings = [x+1 for x in mappings]
-    return mappings
+    mappings_string = '\n'.join(str(element) for element in mappings)
+    return mappings_string
 
 def get_MSA_seq(msa_file, target_id):
     """
     find the MSA entry for a given ID, e.g. `>467818` and return it
     """
+    #print(target_id)
     with open(msa_file, 'r') as file:
         lines = file.readlines()
         for i in range(len(lines)):
