5323cbd20935ac8efcae5bf846d350da99b05534
Meryl Liu <meryl.liu@gmail.com>
2023-06-29 21:19:15 -0700

    Added function to access .pdb files from github repo

---
+++ pymol_surfaceDE.py
@@ -0,0 +1,60 @@
+import pymol
+import argparse
+import os
+import requests
+
+parser = argparse.ArgumentParser(
+                    prog='PyMOL surface D/E residue identification',
+                    description='For a given list of pdb files, generate the surface D/E residues. This script uses the `get_sasa_relative` command from PyMOL with a default cutoff of 0.2',
+                    )
+parser.add_argument('-l', '--list', type=str, required=True, help='Path to the input csv file with `UniProtID, file path, chain ID` ')
+parser.add_argument('-d', '--directory', type=str, required=True, help='Path to the output directory.')
+parser.add_argument('-c', '--cutoff', type=float, default=0.2, help='cutoff for relative per residue solvent accessible area' )
+
+args = parser.parse_args()
+pdb_list = args.list
+output_path = args.directory
+cutoff = args.cutoff
+
+def fetch_pdb_from_github(file_path):
+    raw_url = f"https://raw.githubusercontent.com/{file_path}"
+    response = requests.get(raw_url)
+    if response.status_code == requests.codes.ok:
+        pdb_content = response.text
+        return pdb_content
+    else:
+        print("Failed to fetch PDB file:", response.status_code)
+        return None
+     
+
+def pymol_surf_DE(pdb_file, chain_id, cutoff, protein_id):
+    pymol.cmd.load(pdb_file, 'structure')
+    sasa_dict = pymol.cmd.get_sasa_relative()
+    rel_sa_cutoff = 0.2
+    chain_id = 'A'
+    surface_residues = [int(x[-1]) for x, val in sasa_dict.items() if val > rel_sa_cutoff and x[2] == chain_id]
+    prot_seq = pymol.cmd.get_fastastr('structure and chain {}'.format(chain_id))
+    # prot_seq = pymol.cmd.get_fastastr(key='model_{}'.format(chain_id))
+    prot_seq = ''.join(prot_seq.split('\n')[1:])
+    #print(prot_seq)
+    surf_DE_list = [int(x) for x in surface_residues if prot_seq[x-1] in ['D', 'E']]
+    with open(os.path.join(output_path, '{}_SurfDE.txt'.format(protein_id)), 'w') as f:
+        f.write(','.join(map(str, surf_DE_list)))
+
+pymol.finish_launching(['pymol', '-qc']) 
+
+with open(pdb_list, 'r') as f:
+    for line in f:
+        print(line)
+        prot_id, pdb_file_path, chain_id = line.split(',')
+        prot_id, pdb_file_path, chain_id = prot_id.strip(), pdb_file_path.strip(), chain_id.strip()
+        pdb_file_content = fetch_pdb_from_github(pdb_file_path)
+        pdb_file_name = './{}.pdb'.format(prot_id)
+        with open(pdb_file_name,'w') as f:
+            f.write(pdb_file_content)
+        pymol_surf_DE(pdb_file_name, chain_id, cutoff, prot_id)
+        os.remove(pdb_file_name)
+
+pymol.cmd.quit()
+    
+
